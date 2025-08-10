f070b67c5c22006f0851575b8f5ccd887e522d14
1234Monica <monicaionela@gmail.com>
2025-08-10 12:17:56 +0300

    uopdatecomit

--- src/main/java/org/example/params/Utility.java
+++ src/main/java/org/example/params/Utility.java
@@ -1,14 +1,47 @@
-package params;
+package org.example.params;
 
 public class Utility {
 
+
+    /*
+    Utility mechanisms:
+    -object class
+    -statitc methods
+    -singleton
+     */
+
     // 1. Check if number is odd
+    //odd- impar,even=par
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
 
     // 2. Round a double value to nearest integer
+    public static double roundDouble(double value) {
+        return Math.round(value);
+    }
 
     // 3. Check if a string is palindrome
+    //StringBuilder este weapper peste String, asa cum Integer este peste int
+    // (un  numar sau un  text care de la stanga la dreapta e identic,ex ana)
+    public static boolean isPalindrome(String text) {
+        if (text == null) return false;
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
 
     // 4. Convert string to uppercase
+    // trim - eliminia caracterele albel de la stanga la dreapta stringului -> "   alune    " , "alune"
+
+    public static String toUppercase(String imput) {
+        return imput.trim().toUpperCase();
+
+    }
 
     // 5. Check if an object is an instance of a given class
+    //Object = universal parent for all Java classes ->can override toString ,hashCode
+    public static boolean isInstance(Object obj, Class<?> cls) {
+        return cls.isInstance(obj);
+
+    }
 }
--- src/test/java/org/example/params/UtilityTest.java
+++ src/test/java/org/example/params/UtilityTest.java
@@ -1,4 +1,42 @@
 package org.example.params;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+//asserThat(actual).<verify>(expected
+//verify sa acepete in totdeauna
 
 public class UtilityTest {
+    @ParameterizedTest   //-> needs test data surce
+    @ValueSource(ints = {1,3,5,7,9,-33,-21,6})
+    void test_IsOdd_happy_path(int number) {
+        assertThat(Utility.isOdd(number))
+                .withFailMessage("Numarul trebuie sa fie impar")
+                .isTrue();
+    }
+    @ParameterizedTest
+    @ValueSource(doubles = {2.3,2.5,-2.0,-22.12})
+    void test_roundDouble(  Double imput){
+        double expected = Math.round(imput);   //rotunjeste la integer -> 2,3,2,-2,-22
+        assertThat(Utility.roundDouble(imput))
+                .isEqualTo(expected);
+    }
+    //to do : modify ti check String va Sting
+    @ParameterizedTest
+    @ValueSource(strings= {"121", "madam", "apa", "Ana", "hello"})
+    void test_isPalondrome(String imput) {
+        String expected = new StringBuilder(imput).reverse().toString();
+        assertThat(Utility.isPalindrome(imput))
+                .isEqualTo(expected.equalsIgnoreCase(imput));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"   test; TEST","tEST;TEST", "Java;JAVA"},delimiter = ';')
+    void test_UpperCase(String imput ,String expected) {
+        assertThat(Utility.toUppercase(imput))
+                .isEqualTo(expected);
+    }
+
 }
