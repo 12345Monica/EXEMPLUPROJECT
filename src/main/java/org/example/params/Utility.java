7250b55682163a2929b3698e827900ad33ca8f35
1234Monica <monicaionela@gmail.com>
2025-08-10 12:13:16 +0300

    Initial commit

---
+++ src/main/java/org/example/params/Utility.java
@@ -0,0 +1,14 @@
+package params;
+
+public class Utility {
+
+    // 1. Check if number is odd
+
+    // 2. Round a double value to nearest integer
+
+    // 3. Check if a string is palindrome
+
+    // 4. Convert string to uppercase
+
+    // 5. Check if an object is an instance of a given class
+}
